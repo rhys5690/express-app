3a19e303cf341990398f4cceb0fa57da3a62bc5a
rhys5690 <rhys5690@gmail.com>
2017-07-17 17:11:42 +1000

    .

---
+++ express-app/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<html><head></head><body><h1>Hello World</h1></body></html>');
+});
+
+app.get('/api', (req, res) => {
+  res.json({ firstname: 'Rhys', lastname: 'Diab' });
+});
+
+app.get('/person/:id', (req, res) => {
+  res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body></html>`);
+});
+
+app.listen(port);
