71d26369b72073969e8d96c4a8f15c91865eef56
rhys5690 <rhys5690@gmail.com>
2017-07-18 10:33:32 +1000

    added to app

--- express-app/app.js
+++ express-app/app.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', (req, res, next) => {
+  console.log(`Request URL: ${req.url}`)
+  next();
+});
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('<html><head></head><body><h1>Hello World</h1></body></html>');
+  res.render('index');
+});
+
+app.get('/person/:id', (req, res) => {
+  res.render('person', { ID: req.params.id });
 });
 
 app.get('/api', (req, res) => {
